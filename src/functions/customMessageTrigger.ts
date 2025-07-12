2c27b1107f7b6272c6e813ad209de844cfed205b
nivaldoandrade <nivaldoandradef@gmail.com>
2025-07-12 16:54:25 -0300

    feat: implement custom message triggers with email templates
    - Created customMessageTrigger lambda function
    - Rendered html from email using react email
    - Customized subject and body for sign-up and forgot password flows

---
+++ src/functions/customMessageTrigger.ts
@@ -0,0 +1,26 @@
+import { render } from '@react-email/components';
+import { CustomMessageTriggerEvent } from 'aws-lambda';
+import ForgotPasswordTemplate from '../emails/templates/ForgotPasswordTemplate';
+import SignUpTemplate from '../emails/templates/SignUpTemplate';
+
+export async function handler(event: CustomMessageTriggerEvent) {
+  const code = event.request.codeParameter;
+
+  if (event.triggerSource === 'CustomMessage_ForgotPassword') {
+    const html = await render(
+      ForgotPasswordTemplate({ confirmationCode: code }),
+    );
+
+    event.response.emailSubject = 'Código de recuperação de senha';
+    event.response.emailMessage = html;
+  }
+
+  if (event.triggerSource === 'CustomMessage_SignUp') {
+    const html = await render(SignUpTemplate({ confirmationCode: code }));
+
+    event.response.emailSubject = 'Código de confirmação de email';
+    event.response.emailMessage = html;
+  }
+
+  return event;
+}
