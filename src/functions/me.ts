020bc2521567c14b4d3bb8fd989a8adb82078e41
nivaldoandrade <nivaldoandradef@gmail.com>
2025-07-12 14:52:02 -0300

    feat: protect me lambda function with Cognito JWT authorizer
    - Added Jwt authorizer config in serverless.yml
    - Set IAM permission to allow AdminGetUser on UserPool
    - Retrieved authenticated user email using AdminGetUserCommand

--- src/functions/me.ts
+++ src/functions/me.ts
@@ -1,11 +1,23 @@
-import { APIGatewayProxyEventV2 } from 'aws-lambda';
+import { AdminGetUserCommand } from '@aws-sdk/client-cognito-identity-provider';
+import { APIGatewayProxyEventV2WithJWTAuthorizer } from 'aws-lambda';
+import { cognitoClient } from '../clients/cognitoClient';
 
-// implement lambda function me
-export async function handler(event: APIGatewayProxyEventV2) {
+export async function handler(event: APIGatewayProxyEventV2WithJWTAuthorizer) {
+  const claims = event.requestContext.authorizer.jwt.claims;
+
+  const command = new AdminGetUserCommand({
+    Username: claims.sub as string,
+    UserPoolId: process.env.COGNITO_USER_POOL_ID,
+  });
+
+  const { UserAttributes } = await cognitoClient.send(command);
+
+  const email = UserAttributes?.find(({ Name }) => Name === 'email')?.Value;
+
   return {
     statusCode: 200,
     body: JSON.stringify({
-      me: 'true',
+      email,
     }),
   };
 }
