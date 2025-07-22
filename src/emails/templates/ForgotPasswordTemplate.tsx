40242ea528af9b0f56ba37b88f7280faa2eae71d
nivaldoandrade <nivaldoandradef@gmail.com>
2025-07-22 12:21:16 -0300

    refactor: remove TailwindConfig to avoid lambda cold start issues

--- src/emails/templates/ForgotPasswordTemplate.tsx
+++ src/emails/templates/ForgotPasswordTemplate.tsx
@@ -2,12 +2,12 @@ import {
   Body,
   Container,
   Heading,
+  Html,
   Preview,
   Section,
   Text,
 } from '@react-email/components';
 import React from 'react';
-import { TailwindConfig } from '../components/TailwindConfig';
 
 interface IForgotPasswordTemplate {
   confirmationCode: string;
@@ -17,35 +17,82 @@ export default function ForgotPasswordTemplate({
   confirmationCode,
 }: IForgotPasswordTemplate) {
   return (
-    <TailwindConfig>
-      <Body className="bg-white my-0 mx-auto font-sans">
+    <Html>
+      <Body style={main}>
         <Preview>Código de recuperação de senha</Preview>
-        <Container className="my-0 mx-auto py-0 px-5">
-          <Heading as="h1" className="text-center">
+        <Container style={container}>
+          <Heading as="h1" style={heading}>
             Código de recuperação de senha
           </Heading>
-          <Text className="text-eerie-black leading-7 mb-[30px]">
+          <Text style={heroText}>
             O seu código de recuperação de senha está abaixo - insira-o na
             janela aberta do seu navegador e nós ajudaremos você a fazer o
             recuperação.
           </Text>
 
-          <Section className="bg-gray-200 rounded-sm mb-[30px] py-[40px] px-[10px]">
-            <Text className="text-3xl text-center align-middle">
-              {confirmationCode}
-            </Text>
+          <Section style={sectionCode}>
+            <Text style={codeText}>{confirmationCode}</Text>
           </Section>
 
-          <Text className="text-black text-sm leading-6 text-center">
+          <Text style={footer}>
             Se não solicitou este e-mail, não há motivo para preocupação, pode
             ignorá-lo com segurança.
           </Text>
         </Container>
       </Body>
-    </TailwindConfig>
+    </Html>
   );
 }
 
 ForgotPasswordTemplate.PreviewProps = {
   confirmationCode: '459159',
 } as IForgotPasswordTemplate;
+
+// bg-white my-0 mx-auto font-sans
+const main = {
+  backgroundColor: '#ffffff',
+  margin: '0px auto',
+  fontFamily:
+    'ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji',
+};
+
+// my-0 mx-auto py-0 px-5
+const container = {
+  margin: '0px auto',
+  padding: '0px 16px',
+};
+
+// text-center
+const heading = {
+  textAlign: 'center' as const,
+};
+
+// text-eerie-black leading-7 mb-[30px]
+const heroText = {
+  color: '#1D1C1D',
+  lineHeight: '28px',
+  marginBottom: '30px',
+};
+
+// bg-gray-200 rounded-sm mb-[30px] py-[40px] px-[10px]
+const sectionCode = {
+  backgroundColor: '#e5e7eb',
+  borderRadius: '4px',
+  marginBottom: '30px',
+  padding: '40px 10px',
+};
+
+// text-3xl text-center align-middle
+const codeText = {
+  fontSize: '30px',
+  textAlign: 'center' as const,
+  verticalAlign: 'middle',
+};
+
+// text-black text-sm leading-6 text-center
+const footer = {
+  fontSize: '14px',
+  color: 'black',
+  lineHeight: '24px',
+  textAlign: 'center' as const,
+};
--- src/emails/templates/SignUpTemplate.tsx
+++ src/emails/templates/SignUpTemplate.tsx
@@ -2,12 +2,12 @@ import {
   Body,
   Container,
   Heading,
+  Html,
   Preview,
   Section,
   Text,
 } from '@react-email/components';
 import React from 'react';
-import { TailwindConfig } from '../components/TailwindConfig';
 
 interface ISignInTemplate {
   confirmationCode: string;
@@ -15,30 +15,69 @@ interface ISignInTemplate {
 
 export default function SignUpTemplate({ confirmationCode }: ISignInTemplate) {
   return (
-    <TailwindConfig>
-      <Body className="bg-white my-0 mx-auto font-sans">
+    <Html>
+      <Body style={main}>
         <Preview>Código de confirmação de email</Preview>
-        <Container className="my-0 mx-auto py-0 px-5">
-          <Heading as="h1" className="text-center">
+        <Container style={container}>
+          <Heading as="h1" style={heading}>
             Código de confirmação de email
           </Heading>
-          <Text className="text-eerie-black leading-7 mb-[30px]">
+          <Text style={heroText}>
             O seu código de confirmação de email está abaixo - insira-o na
             janela aberta do seu navegador e nós ajudaremos você a fazer a
             confirmação.
           </Text>
 
-          <Section className="bg-gray-200 rounded-sm mb-[30px] py-[40px] px-[10px]">
-            <Text className="text-3xl text-center align-middle">
-              {confirmationCode}
-            </Text>
+          <Section style={sectionCode}>
+            <Text style={codeText}>{confirmationCode}</Text>
           </Section>
         </Container>
       </Body>
-    </TailwindConfig>
+    </Html>
   );
 }
 
 SignUpTemplate.PreviewProps = {
   confirmationCode: '459159',
 } as ISignInTemplate;
+
+// bg-white my-0 mx-auto font-sans
+const main = {
+  backgroundColor: '#ffffff',
+  margin: '0px auto',
+  fontFamily:
+    'ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji',
+};
+
+// my-0 mx-auto py-0 px-5
+const container = {
+  margin: '0px auto',
+  padding: '0px 16px',
+};
+
+// text-center
+const heading = {
+  textAlign: 'center' as const,
+};
+
+// text-eerie-black leading-7 mb-[30px]
+const heroText = {
+  color: '#1D1C1D',
+  lineHeight: '28px',
+  marginBottom: '30px',
+};
+
+// bg-gray-200 rounded-sm mb-[30px] py-[40px] px-[10px]
+const sectionCode = {
+  backgroundColor: '#e5e7eb',
+  borderRadius: '4px',
+  marginBottom: '30px',
+  padding: '40px 10px',
+};
+
+// text-3xl text-center align-middle
+const codeText = {
+  fontSize: '30px',
+  textAlign: 'center' as const,
+  verticalAlign: 'middle',
+};
