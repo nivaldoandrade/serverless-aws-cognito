cd1f29f2aedc924065ad8b37ad6e680cbeca727e
nivaldoandrade <nivaldoandradef@gmail.com>
2025-07-23 16:50:56 -0300

    feat: add TABLE_NAME to env config

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,6 +4,7 @@ const envSchema = z.object({
   COGNITO_CLIENT_ID: z.string().check(z.minLength(1)),
   COGNITO_CLIENT_SECRET: z.string().check(z.minLength(1)),
   COGNITO_USER_POOL_ID: z.string().check(z.minLength(1)),
+  TABLE_NAME: z.string().check(z.minLength(1)),
 });
 
 export const env = envSchema.parse(process.env);
