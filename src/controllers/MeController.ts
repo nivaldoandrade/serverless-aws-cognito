ab94b053ea1b28e30c051ef53f797e37ba317778
nivaldoandrade <nivaldoandradef@gmail.com>
2025-07-27 18:07:40 -0300

    refactor: replace AdminGetUserCommand with QueryCommand
    - Extracted user data from ACCOUNT and PROFILE items

--- src/controllers/MeController.ts
+++ src/controllers/MeController.ts
@@ -1,5 +1,5 @@
-import { AdminGetUserCommand } from '@aws-sdk/client-cognito-identity-provider';
-import { cognitoClient } from '../clients/cognitoClient';
+import { QueryCommand } from '@aws-sdk/lib-dynamodb';
+import { dynamoClient } from '../clients/dynamoClient';
 import { env } from '../config/env';
 import {
   ControllerResponse,
@@ -8,21 +8,42 @@ import {
 } from './type/IController';
 
 export class MeController implements IController {
-  async execute({ userId }: IControllerRequest): Promise<ControllerResponse> {
-    const command = new AdminGetUserCommand({
-      Username: userId,
-      UserPoolId: env.COGNITO_USER_POOL_ID,
+  async execute({
+    accountId,
+  }: IControllerRequest): Promise<ControllerResponse> {
+    const command = new QueryCommand({
+      TableName: env.TABLE_NAME,
+      Limit: 2,
+      ProjectionExpression: '#email, #name, #type',
+      KeyConditionExpression: '#PK = :PK',
+      ExpressionAttributeNames: {
+        '#PK': 'PK',
+        '#email': 'email',
+        '#name': 'name',
+        '#type': 'type',
+      },
+      ExpressionAttributeValues: {
+        ':PK': `ACCOUNT#${accountId}`,
+      },
     });
 
-    const { UserAttributes } = await cognitoClient.send(command);
+    const { Items = [] } = await dynamoClient.send(command);
 
-    const email = UserAttributes?.find(({ Name }) => Name === 'email')?.Value;
+    const profile = Items.find((item) => item.type === 'PROFILE');
+    const account = Items.find((item) => item.type === 'ACCOUNT');
+
+    if (!profile || !account) {
+      throw new Error('Account not found.');
+    }
+
+    const user = {
+      name: profile.name,
+      email: account.email,
+    };
 
     return {
       statusCode: 200,
-      body: {
-        email,
-      },
+      body: user,
     };
   }
 }
