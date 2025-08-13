5a4774262fc30239776c13c520ad15f7749d01c4
nivaldoandrade <nivaldoandradef@gmail.com>
2025-08-13 17:17:55 -0300

    test: add unit tests for SignInController

---
+++ src/controllers/__tests__/SignInController.test.ts
@@ -0,0 +1,64 @@
+import {
+  AuthenticationResultType,
+  InitiateAuthCommand,
+} from '@aws-sdk/client-cognito-identity-provider';
+import { describe } from 'node:test';
+import { $ZodError } from 'zod/v4/core';
+import { mockCognitoSend } from '../../tests/mocks/awsClients';
+import { SignInController } from '../SignInController';
+
+describe('SignInController', () => {
+  let controller: SignInController;
+
+  beforeEach(() => {
+    controller = new SignInController();
+    jest.clearAllMocks();
+  });
+
+  const body = {
+    email: 'john.doe@mail.com',
+    password: '12345678',
+  };
+
+  test('should be sign in successfully and return tokens', async () => {
+    const mockAuthenticationResult = {
+      AccessToken: 'accesToken',
+      RefreshToken: 'refreshToken',
+    } as AuthenticationResultType;
+
+    mockCognitoSend.mockResolvedValueOnce({
+      AuthenticationResult: mockAuthenticationResult,
+    });
+
+    const response = await controller.execute({ body });
+
+    expect(mockCognitoSend).toHaveBeenCalledWith(
+      expect.any(InitiateAuthCommand),
+    );
+
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toEqual({
+      accessToken: 'accesToken',
+      refreshToken: 'refreshToken',
+    });
+  });
+
+  test('should be throw error when cognito does not return tokens', async () => {
+    mockCognitoSend.mockResolvedValueOnce({});
+
+    await expect(controller.execute({ body })).rejects.toThrow(
+      `Error in cognito to signin user: ${body.email}`,
+    );
+  });
+
+  test('should be validate input schema and throw if invalid', async () => {
+    const invalideBody = {
+      email: '',
+      password: '12345678',
+    };
+
+    await expect(controller.execute({ body: invalideBody })).rejects.toThrow(
+      $ZodError,
+    );
+  });
+});
